0b4a83a0d4caed21dc9c3192571a04f337e0fc42
Nathank4242 <nkweon@gmail.com>
2024-11-12 13:55:45 -0500

    vite

--- app/js/index.js
+++ app/js/index.js
@@ -42,3 +42,35 @@ function filterPokemonCharacters() {
 document
   .getElementById("filterPokemonButton")
   .addEventListener("click", filterPokemonCharacters);
+
+function filterMarioCharacters() {
+  const marioCharacters = melee.filter(
+    (character) => character.game_of_origin === "Super Mario"
+  );
+  createCards(marioCharacters);
+}
+document
+  .getElementById("filterMarioButton")
+  .addEventListener("click", filterMarioCharacters);
+
+function filterFireCharacters() {
+  const FireCharacters = melee.filter(
+    (character) => character.game_of_origin === "Fire Emblem"
+  );
+  createCards(FireCharacters);
+}
+document
+  .getElementById("filterFireButton")
+  .addEventListener("click", filterFireCharacters);
+
+function filterOtherCharacters() {
+  const OtherCharacters = melee.filter(
+    (character) =>
+      character.game_of_origin === "Kirby" &&
+      character.game_of_origin === "F-Zero"
+  );
+  createCards(OtherCharacters);
+}
+document
+  .getElementById("filterOtherButton")
+  .addEventListener("click", filterOtherCharacters);
